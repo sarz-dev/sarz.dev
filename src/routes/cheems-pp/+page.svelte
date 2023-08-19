5521163fed460aefc2c5c81bbb3a625e50db9ed9
Sebastián Ariel Rodríguez Zbinden � <sebastian.rzbinden@gmail.com>
2023-08-19 19:52:04 -0400

    First commit

---
+++ src/routes/cheems-pp/+page.svelte
@@ -0,0 +1,83 @@
+<div class="container">
+	<h1>Política de Privacidad de Cheems Samurai</h1>
+	<p>
+		Bienvenido a la Política de Privacidad de la aplicación móvil <b>Cheems Samurai</b>. En esta
+		política, describiremos cómo se maneja la información en nuestra aplicación, así como el
+		compromiso que tenemos con la privacidad de nuestros usuarios. La seguridad y la protección de
+		tus datos son de suma importancia para nosotros. Al utilizar la aplicación
+		<b>Cheems Samurai</b>, aceptas las prácticas descritas en esta política.
+	</p>
+	<h2>Información Recopilada</h2>
+	<p>
+		La aplicación <b>Cheems Samurai</b> está diseñada de manera que no recopila ni almacena ningún tipo
+		de información personal o sensible de los usuarios. No recopilamos nombres, direcciones de correo
+		electrónico, números de teléfono u otra información personal. No accedemos a tu cámara, micrófono,
+		ubicación ni a ningún otro dato personal en tu dispositivo.
+	</p>
+	<h2>Uso de Permisos</h2>
+	<p>
+		Nuestra aplicación solo solicita los permisos necesarios para su funcionamiento. Estos permisos
+		son exclusivamente para garantizar una experiencia de juego fluida y no se utilizan para
+		recopilar información personal. No almacenamos ni transmitimos ningún dato de los usuarios.
+	</p>
+	<h2>Cookies y Tecnologías Similares</h2>
+	<p>
+		La aplicación <b>Cheems Samurai</b> no utiliza cookies ni tecnologías similares para rastrear la
+		actividad de los usuarios. No recopilamos información sobre tus hábitos de navegación ni realizamos
+		seguimiento en línea.
+	</p>
+	<h2>Enlaces Externos</h2>
+	<p>
+		Nuestra aplicación puede contener enlaces a sitios web externos, como nuestras redes sociales u
+		otras plataformas. Sin embargo, una vez que abandonas nuestra aplicación y accedes a sitios
+		externos, esta política de privacidad ya no se aplica. Te recomendamos revisar las políticas de
+		privacidad de esos sitios antes de proporcionar cualquier información.
+	</p>
+	<h2>Cambios en la Política de Privacidad</h2>
+	<p>
+		Nos reservamos el derecho de actualizar o modificar esta Política de Privacidad en cualquier
+		momento. Cualquier cambio será publicado en esta página y la fecha de la última actualización se
+		modificará al final del documento. Te recomendamos revisar esta política periódicamente para
+		estar al tanto de cómo protegemos la información de los usuarios.
+	</p>
+	<h2>Contacto</h2>
+	<p>
+		Si tienes alguna pregunta o inquietud sobre nuestra Política de Privacidad o sobre el manejo de
+		datos en la aplicación <b>Cheems Samurai</b>, no dudes en ponerte en contacto con nosotros a
+		través de <a href="mailto:sarz-dev1@gmail.com">sarz-dev1@gmail.com</a>.
+	</p>
+	<p>
+		Gracias por confiar en <b>Cheems Samurai</b>. Tu privacidad es importante para nosotros y
+		trabajamos arduamente para asegurarnos de que tu experiencia sea segura y agradable.
+	</p>
+	<p>Fecha de última actualización: 19-08-2023</p>
+</div>
+
+<style>
+	.container {
+		max-width: 800px;
+		margin: 50px auto;
+		padding: 1em 3em;
+		background-color: #fff;
+		border-radius: 5px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+
+	h1 {
+		color: #4caf50;
+	}
+
+	p {
+		margin-bottom: 20px;
+		line-height: 1.6;
+	}
+
+	a {
+		color: #4caf50;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
