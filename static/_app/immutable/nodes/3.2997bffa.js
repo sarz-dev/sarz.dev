f4f8e075c73c3ecf65ec0206b50d0620f6eba191
Sebastián Ariel Rodríguez Zbinden � <sebastian.rzbinden@gmail.com>
2023-08-19 20:44:03 -0400

    config for gh pages

---
+++ static/_app/immutable/nodes/3.2997bffa.js
@@ -0,0 +1,21 @@
+import{s as r,n as s}from"../chunks/scheduler.e108d1fd.js";import{S as t,i as c,g as l,h as d,x as m,k as p,a as u,f as v}from"../chunks/index.5ae4157e.js";function f(o){let a,i=`<h1 class="svelte-10sxf2s">Política de Privacidad de Cheems Samurai</h1> <p class="svelte-10sxf2s">Bienvenido a la Política de Privacidad de la aplicación móvil <b>Cheems Samurai</b>. En esta
+		política, describiremos cómo se maneja la información en nuestra aplicación, así como el
+		compromiso que tenemos con la privacidad de nuestros usuarios. La seguridad y la protección de
+		tus datos son de suma importancia para nosotros. Al utilizar la aplicación
+		<b>Cheems Samurai</b>, aceptas las prácticas descritas en esta política.</p> <h2>Información Recopilada</h2> <p class="svelte-10sxf2s">La aplicación <b>Cheems Samurai</b> está diseñada de manera que no recopila ni almacena ningún tipo
+		de información personal o sensible de los usuarios. No recopilamos nombres, direcciones de correo
+		electrónico, números de teléfono u otra información personal. No accedemos a tu cámara, micrófono,
+		ubicación ni a ningún otro dato personal en tu dispositivo.</p> <h2>Uso de Permisos</h2> <p class="svelte-10sxf2s">Nuestra aplicación solo solicita los permisos necesarios para su funcionamiento. Estos permisos
+		son exclusivamente para garantizar una experiencia de juego fluida y no se utilizan para
+		recopilar información personal. No almacenamos ni transmitimos ningún dato de los usuarios.</p> <h2>Cookies y Tecnologías Similares</h2> <p class="svelte-10sxf2s">La aplicación <b>Cheems Samurai</b> no utiliza cookies ni tecnologías similares para rastrear la
+		actividad de los usuarios. No recopilamos información sobre tus hábitos de navegación ni realizamos
+		seguimiento en línea.</p> <h2>Enlaces Externos</h2> <p class="svelte-10sxf2s">Nuestra aplicación puede contener enlaces a sitios web externos, como nuestras redes sociales u
+		otras plataformas. Sin embargo, una vez que abandonas nuestra aplicación y accedes a sitios
+		externos, esta política de privacidad ya no se aplica. Te recomendamos revisar las políticas de
+		privacidad de esos sitios antes de proporcionar cualquier información.</p> <h2>Cambios en la Política de Privacidad</h2> <p class="svelte-10sxf2s">Nos reservamos el derecho de actualizar o modificar esta Política de Privacidad en cualquier
+		momento. Cualquier cambio será publicado en esta página y la fecha de la última actualización se
+		modificará al final del documento. Te recomendamos revisar esta política periódicamente para
+		estar al tanto de cómo protegemos la información de los usuarios.</p> <h2>Contacto</h2> <p class="svelte-10sxf2s">Si tienes alguna pregunta o inquietud sobre nuestra Política de Privacidad o sobre el manejo de
+		datos en la aplicación <b>Cheems Samurai</b>, no dudes en ponerte en contacto con nosotros a
+		través de <a href="mailto:sarz-dev1@gmail.com" class="svelte-10sxf2s">sarz-dev1@gmail.com</a>.</p> <p class="svelte-10sxf2s">Gracias por confiar en <b>Cheems Samurai</b>. Tu privacidad es importante para nosotros y
+		trabajamos arduamente para asegurarnos de que tu experiencia sea segura y agradable.</p> <p class="svelte-10sxf2s">Fecha de última actualización: 19-08-2023</p>`;return{c(){a=l("div"),a.innerHTML=i,this.h()},l(e){a=d(e,"DIV",{class:!0,"data-svelte-h":!0}),m(a)!=="svelte-yrvd12"&&(a.innerHTML=i),this.h()},h(){p(a,"class","container svelte-10sxf2s")},m(e,n){u(e,a,n)},p:s,i:s,o:s,d(e){e&&v(a)}}}class g extends t{constructor(a){super(),c(this,a,null,f,r,{})}}export{g as component};
